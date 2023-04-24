74498bcc8cbf50102d8353a7d67de23415e73d4f
Rene Markovič <41106629+ReneMarkovic@users.noreply.github.com>
2023-04-24 18:18:56 +0200

    init

---
+++ 02_Numerično_odvajanje/Header.h
@@ -0,0 +1,30 @@
+double f(double x) {
+	return sin(x);
+}
+double dfdx(double x) {
+	return cos(x);
+}
+//Numeriène metode za doloèevanje odvoda
+double df_en1(double x, double h) {
+	return (f(x + h) - f(x)) / h;
+}
+double df_en2(double x, double h) {
+	double st = f(x + h) - f(x - h);
+	double im = 2 * h;
+	return st / im;
+}
+double df_en3(double x, double h) {
+	double st = f(x + h) + f(x - h) - 2 * f(x);
+	double im =  4*h;
+	return st / im;
+}
+
+double fabs(double v1, double v2) {
+	double delta = v1 - v2;
+	return sqrt(delta * delta);
+}
+
+double faktor(double start, double stop, int steps) {
+	double eksponent = 1.0 / (double)(steps - 1);
+	return pow(stop / start, eksponent);
+}
---
+++ 02_Numerično_odvajanje/main.cpp
@@ -0,0 +1,68 @@
+﻿// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Header.h"
+
+#define start 1E-6
+#define stop 1
+#define steps 20 
+
+#define PI 4*atan(1)
+
+#define x_start 0
+#define x_stop 2.0*PI
+#define N 100
+
+double a = 0.5;// V tej točki računamo vrednost odvoda.
+double dh = faktor(start, stop, steps);
+
+FILE* data;
+
+int main(){
+	printf("Pi = %f\n", PI);
+	//Naloga 1
+	fopen_s(&data, "naloga_1.txt", "w+");
+	fclose(data);
+
+	double analiticna_vrednost_odvoda = dfdx(a);
+	double h = start;
+	while (h <= stop) {
+		fopen_s(&data, "naloga_1.txt", "a");
+		
+		fprintf(data, "%f\t", h);
+		fprintf(data, "%f\t", analiticna_vrednost_odvoda);
+		fprintf(data, "%f\t%f\t", df_en1(a, h), df_en2(a, h));
+		fprintf(data, "%f\t", fabs(df_en1(a, h),analiticna_vrednost_odvoda));
+		fprintf(data, "%f\n", fabs(df_en2(a, h), analiticna_vrednost_odvoda));
+		
+		fclose(data);
+		h *= dh;
+	}
+
+
+	//Naloga 2
+	double x;
+	double dx = (x_stop - x_start) / (double)(N);
+	h = 0.01;
+
+	fopen_s(&data, "naloga_2.txt", "w+");
+	fclose(data);
+
+	for (x = x_start; x <= x_stop; x += dx) {
+		fopen_s(&data, "naloga_2.txt", "a");
+
+		analiticna_vrednost_odvoda = dfdx(x);
+
+		fprintf(data, "%f\t", x);
+		fprintf(data, "%f\t", analiticna_vrednost_odvoda);
+		fprintf(data, "%f\t%f\t", df_en1(x, h), df_en2(x, h));
+		fprintf(data, "%f\t", fabs(df_en1(x, h), analiticna_vrednost_odvoda));
+		fprintf(data, "%f\n", fabs(df_en2(x, h), analiticna_vrednost_odvoda));
+
+		fclose(data);
+	}
+	return 0;
+}
